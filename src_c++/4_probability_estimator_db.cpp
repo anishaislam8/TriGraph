25243e6b80ad32399f28ce72d1373af77b838c14
anishaislam8 <anishaislam8@gmail.com>
2024-06-20 14:29:42 -0600

    Updated db file

--- src_c++/4_probability_estimator_db.cpp
+++ src_c++/4_probability_estimator_db.cpp
@@ -141,7 +141,9 @@ int main(){
 
             start = clock();
             for (auto node: nodes_test){
-                // get all three_node_subgraphs that contain this node
+                // This node is in the graph (source or destination)
+                // and there is going to be a three node subgraph that contains this node 
+                
                 vector<vector<string> > three_node_subgraphs_containing_this_node;
                 for (auto subgraph: three_node_subgraphs_test){
                     if (find(subgraph.begin(), subgraph.end(), node) != subgraph.end()){
@@ -149,72 +151,8 @@ int main(){
                     }
                 }
 
-                string true_token = object_dict_test.at(node);
-                
-                // I am calling the predict function for each three_node_subgraph that contains this node
-                for (auto subgraph: three_node_subgraphs_containing_this_node){
-                    
-                    vector<string> subgraph_nodes = subgraph;
-                    set<string> subgraph_nodes_set(subgraph.begin(), subgraph.end());
-                    vector<vector<string> > subgraph_edges;
-                    vector<vector<string> > edges_of_original_graph = G_directed_test.get_edges();
-
-                    // this is okay, if edge is (0,0) and nodes set is (0,1,2) -> add this edge
-                    for (const auto& edge : edges_of_original_graph) {
-                        if (subgraph_nodes_set.count(edge[0]) > 0 && subgraph_nodes_set.count(edge[1]) > 0) {
-                            subgraph_edges.push_back(edge);
-                        }
-                    }
-
-
-                    // create node to add list for this subgraph
-
-                    vector<string> two_nodes;
-                    for (auto item: subgraph){
-                        if (item != node){ // take the ones except for this one
-                            two_nodes.push_back(object_dict_test.at(item));
-                        }
-                    }
-
-
-                    // find the third node in three_node_subgraphs_sorted_by_object_dict which has these two nodes
-                    set<string> node_to_add_list;
-
-                    for (auto subgraph_inner: three_node_subgraphs_sorted_by_object_dict){
-                        vector<string> result;
-                        if (two_nodes[0] == two_nodes[1]){
-                            if (count(subgraph_inner.begin(), subgraph_inner.end(), two_nodes[0]) >= 2){
-                                result = find_the_set_difference(subgraph_inner, two_nodes);
-                                node_to_add_list.insert(result[0]);
-                            }
-                        }
-                        else{
-                            if (count(subgraph_inner.begin(), subgraph_inner.end(), two_nodes[0]) >= 1 && count(subgraph_inner.begin(), subgraph_inner.end(), two_nodes[1]) >= 1){
-                                result = find_the_set_difference(subgraph_inner, two_nodes);
-                                node_to_add_list.insert(result[0]);
-                            }
-
-                        }
-                    } 
-                    
-                    if (node_to_add_list.size() == 0){
-                        // iterate through the entire vocabulary, so node to add list is unique_tokens_train
-                        for (auto token: unique_tokens_train){
-                            node_to_add_list.insert(token);
-                        }
-                    }
-                    
-                    rank = predict(object_dict_test, frequency_1_gram, frequency_2_grams, frequency_3_grams, node_to_add_list, node, sum_frequency_1_gram, sum_frequency_2_grams, sum_frequency_3_grams, unique_tokens_train_map, subgraph_nodes, subgraph_edges, true_token);
-
-                    // write to the file the subgraph nodes, blank nodes, rank
-                    for (auto sub_node: subgraph_nodes){
-                        myfile_output << object_dict_test.at(sub_node) << " ";
-                    }
-
-                    myfile_output << object_dict_test.at(node) << " " << rank << endl;
-                    
-                }
-
+                rank = predict(three_node_subgraphs_containing_this_node, three_node_subgraphs_sorted_by_object_dict, object_dict_test, frequency_1_gram, frequency_2_grams, frequency_3_grams, node, sum_frequency_1_gram, sum_frequency_2_grams, sum_frequency_3_grams, unique_tokens_train_map, G_directed_test);
+                myfile_output << node << " " << three_node_subgraphs_containing_this_node.size() << " " << object_dict_test.at(node)  " " << rank << endl;
             }
 
             end = clock();
