2b527e6cf61a2f2cc0bdac5356151c58a9803c5b
anishaislam8 <anishaislam8@gmail.com>
2024-06-30 00:49:28 -0600

    Add edge mrr data

--- src_c++/mrr.py
+++ src_c++/mrr.py
@@ -1,76 +1,59 @@
-# read the first 2000 lines from ../test_hashes/test_hashes_second_half_1.txt
-hashes = []
+# hashes = []
 
-with open('../test_hashes/test_hashes_second_half_1.txt', 'r') as f:
-    lines = f.readlines()
-    i = 0
-    for line in lines:
-        hashes.append(line.strip())
-        i +=1
-        if i == 2000:
-            break
-
-with open('../test_hashes/test_hashes_second_half_2.txt', 'r') as f:
-    lines = f.readlines()
-    i = 0
-    for line in lines:
-        hashes.append(line.strip())
-        i +=1
-        if i == 2000:
-            break
-
-with open('../test_hashes/test_hashes_second_half_3.txt', 'r') as f:
-    lines = f.readlines()
-    i = 0
-    for line in lines:
-        hashes.append(line.strip())
-        i +=1
-        if i == 2000:
-            break
-
-
-with open('../test_hashes/test_hashes_second_half_4.txt', 'r') as f:
-    lines = f.readlines()
-    i = 0
-    for line in lines:
-        hashes.append(line.strip())
-        i +=1
-        if i == 2000:
-            break
+# with open('/media/baguette/aislam4/paths/train_test_split/test_hashes_filtered.txt', 'r') as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         hashes.append(line.strip())
 
 
 
-folder = "rank"
 
-# for each hash in hash, read the rank/hash file
+# folder = "edge_rank"
 
-mrr = {}
-for hash in hashes:
-    try:
-        with open(f'{folder}/{hash}.txt', 'r') as f:
-            mrr_of_hash = 0.0
-            lines = f.readlines()
-            for line in lines:
-                line = line.strip()
-                # PD-ROOT_obj-0 7 select 5
-                rank = int(line.split(" ")[-1])
-                if rank == -1:
-                    continue
-                mrr_of_hash += (1.0 / (rank*1.0))
-            mrr[hash] = mrr_of_hash / len(lines)
-    except:
-        pass
+# # for each hash in hash, read the rank/hash file
+
+# mrr = {}
+# for hash in hashes:
+#     try:
+#         with open(f'{folder}/{hash}.txt', 'r') as f:
+#             mrr_of_hash = 0.0
+#             lines = f.readlines()
+#             if len(lines) == 0: # no connections
+#                 continue
+#             for line in lines:
+#                 line = line.strip()
+#                 # PD-ROOT_obj-0 7 select 5
+#                 rank = int(line.split(" ")[-1])
+#                 if rank == -1:
+#                     continue
+#                 mrr_of_hash += (1.0 / (rank*1.0))
+#             mrr[hash] = mrr_of_hash / len(lines)
+#     except:
+#         pass
+
+# write the mrr to a file
+# with open(f'edge_mrr.txt', 'w') as f:
+#     for hash, mrr_value in mrr.items():
+#         f.write(f'{hash} {mrr_value}\n')
+
 
 
 # print the highest mrr, lowest mrr and average mrr, also, generate a histogram of mrr
-mrr_values = list(mrr.values())
+
+# read the edge_mrr_complete.txt file
+mrr_values = []
+with open('edge_mrr_completed.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        mrr_values.append(float(line.strip().split(" ")[1]))
+
 
 import matplotlib.pyplot as plt
 
 plt.hist(mrr_values, color="lightgreen", ec = "black", bins=20)
 plt.xlabel('MRR')
-plt.ylabel('Frequency')
-plt.title('Histogram of MRR of 7626 Test Graphs')
+plt.ylabel('Frequency of Test Graphs')
+plt.title('Histogram of Edge Prediction MRR for Test Graphs')
 plt.show()
 
 
@@ -100,9 +83,6 @@ import pandas as pd
 df = pd.DataFrame(mrr_values, columns=['MRR'])
 print(df.describe())
 
-# write the mrr to a file
-with open(f'mrr.txt', 'w') as f:
-    for hash, mrr_value in mrr.items():
-        f.write(f'{hash} {mrr_value}\n')
+
 
 
