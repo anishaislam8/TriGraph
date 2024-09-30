b5f705861122d9c9424f61da0570c30c680b65de
anishaislam8 <anishaislam8@gmail.com>
2024-09-30 01:57:41 -0600

    Rename

--- src_c++/test.sh
+++ src_c++/test.sh
@@ -2,25 +2,25 @@ echo "Running tests..."
 echo
 
 echo "Testing corpus creation of 1, 2, 3 grams..."
-g++ -O3 -fconcepts -o test.exe step1-3_test.cpp utils.cpp graph.cpp -lsqlite3
+g++ -O3 -fconcepts -o test.exe test_corpus_creation.cpp utils.cpp graph.cpp -lsqlite3
 ./test.exe
 echo
 
 
 echo "Score test..."
-g++ -O3 -fconcepts -o test.exe score_test.cpp utils.cpp graph.cpp -lsqlite3
+g++ -O3 -fconcepts -o test.exe test_score.cpp utils.cpp graph.cpp -lsqlite3
 ./test.exe
 echo
 
 
 echo "Utils test..."
-g++ -O3 -fconcepts -o test.exe utils_test.cpp utils.cpp graph.cpp -lsqlite3
+g++ -O3 -fconcepts -o test.exe test_utils.cpp utils.cpp graph.cpp -lsqlite3
 ./test.exe
 echo
 
 
 echo "Heap test..."
-g++ -O3 -fconcepts -o test.exe heap_test.cpp utils.cpp graph.cpp -lsqlite3
+g++ -O3 -fconcepts -o test.exe test_heap.cpp utils.cpp graph.cpp -lsqlite3
 ./test.exe
 echo
 
